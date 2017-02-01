345eaa1168138524698eb41c422ff91db4e05697
wesley santos <fwsantos23@gmail.com>
2017-02-01 17:22:12 -0200

    Exercicio Finalizado

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Curso laravel</title>
+</head>
+<body>
+<?php 
+	foreach (range(1, 5) as $number) {
+		echo "<h".$number."> Letra H".$number."<br></h".$number.">";
+	}
+
+ ?>
+
+</body>
+</html>
