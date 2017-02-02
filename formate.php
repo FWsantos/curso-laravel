ef050ab674275f6566db42e2ac9d3f28134d4dd2
wesley santos <fwsantos23@gmail.com>
2017-02-02 17:57:28 -0200

    exercicio a terminar

---
+++ formate.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+	body{
+		text
+	}
+</style>
+	<title>Curso Laravel</title>
+</head>
+<body>
+	<?php 
+			echo "<p style='color:"+$_PDO["cor"]+";text-align:"+$_POST["alinhamento"]+";size:"+$_POST["size"]+"'>"+$_POST[];
+	
+	?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Curso laravel</title>
+</head>
+<body>
+	<form method="post" action="formate.php">
+		<input type="text" name="txttexto"><button>Formatar</button><br>
+
+		<select name="cor">
+			<option value="blue">Azul</option>
+			<option value="red">Vermelho</option>
+		</select>
+		<br>
+		<select name="alinhamento">
+			<option value="left">Alinhar a esquerda</option>
+			<option value="right">Alinhar a direita</option>
+		</select>
+		<br>
+		Tamanho: <input type="number" name="size">
+		<br>
+		<label for="repeticao">Número de Repetições: </label><input type="number" name="repeticao" id="repeticao">
+		<br>
+	</form>
+
+	
+
+
+	
+</body>
+</html>
